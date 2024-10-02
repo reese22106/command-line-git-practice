11be8ec26eca8b320d960c30179c44885daff2d0
Reese <rfair@uw.edu>
2024-10-02 16:34:28 -0700

    2nd commit

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -2,7 +2,7 @@
 # This is a practice script for the Husky Coding Project's Git
 # and Command Line workshop. 
 
-NAME="Reese Fairchild" # Change This Variable to Your Name
+NAME="Reese O. Fairchild" # Change This Variable to Your Name
 
 validate_name() {
     if [ "$NAME" = "HELLO WORLD" ]; then
